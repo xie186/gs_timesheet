0655f14494fb279b51e9ab17fb13d2fb7947c19e
xie186 <xie186@purdue.edu>
2021-07-12 14:12:57 -0400

    Update test.py

--- test.py
+++ test.py
@@ -1,7 +1,49 @@
+#!/usr/bin/env python
+#-*-- coding: utf-8 -*-
+import argparse
 import gspread
 
 gc = gspread.service_account()
 
-sh = gc.open("Timesheet_2020")
 
-print(sh.sheet1.get('Jan'))
+#date_format = '%m/%d/%Y'
+date_format = '%Y-%m-%d %H:%M:%S'
+
+def validate(date_string):
+    #date_format = '%m/%d/%Y'
+    try:
+        date_obj = datetime.strptime(date_string, date_format)
+        return True
+    except ValueError:
+        return False
+
+def gspread(options):
+    sh = gc.open_by_key(options.id)
+    month_list = options.months.split(",")
+    date_now = 0 
+    for month in month_list:
+        worksheet = sh.worksheet(month)
+        data = worksheet.get_all_values()
+        for row in data:
+            #print(type(row[0]))
+            if not row[0]  and row[1]  and not row[2]:
+                #print(type(row[1].value))
+                date_string = str(row[1])
+                #print(str(row[1].value) + " " + str(validate(date_string)))
+                date_string_val = date_string.split(" ")
+                date_now = date_string_val[0]
+                #print(date_now)
+            if not row[0]  and not row[6]  and row[2]  and "Summary" not in str(row[6]):
+                new_row =[date_now, row[1], row[2], row[5]];
+                print("\t".join(new_row)) 
+          
+
+if __name__ == '__main__':
+    ## description - Text to display before the argument help (default: none)
+    parser = argparse.ArgumentParser(add_help=False)                                 
+    parser.add_argument('-i', '--id', help='sheet id', required=True) 
+    parser.add_argument('-m', '--months', help='months', required=True)
+    parser.add_argument('-k', '--keyword', help='keyword', required=True) 
+    parser.add_argument('-o', '--output', help='output file', default = "Output_file.tab") 
+    options = parser.parse_args()
+    gspread(options)
